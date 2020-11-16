d19c9d915a4c4c331f5a940729ce40b415c7eb31
Your Name <renato_inovacao@hotmail.com>
2020-11-16 01:20:21 -0300

    Exerc 03 Feito

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Excercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exe-03 Aumento de salario para os funcionários");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite o salário do funcionário:");
+            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+             if(salario >= 500.00) {
+                double aumento = salario * 0.3;
+                double novoSalario = salario + aumento;
+
+                Console.WriteLine("O salário será reajustado para: " + novoSalario + " reais.");
+
+             }else{
+                 Console.WriteLine("O salárionão será reajustado.");
+             }
+
+           
+        }
+    }
+}
